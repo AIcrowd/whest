c7df69d03c51893d4cc9838f7356781880542bdf
Sharada Mohanty <mohanty@aicrowd.com>
2026-04-01 16:36:40 +0100

    feat: add FFT operations (14 transforms + 4 free ops)
    Implements Task 6: FFT Transforms + Free Ops using Cooley-Tukey radix-2
    cost model (5*N*ceil(log2(N)) for complex, 5*(N//2)*ceil(log2(N)) for real).
    Updates registry entries from blacklisted to counted_custom/free. Fixes
    Python 3.8 compatibility in errors.py by adding from __future__ import annotations.

--- src/mechestim/fft/__init__.py
+++ src/mechestim/fft/__init__.py
@@ -1,8 +1,22 @@
-"""FFT submodule stub for mechestim.
-
-All numpy.fft functions are currently blacklisted because their
-O(n log n) cost model has not been implemented.
-"""
+"""FFT submodule for mechestim."""
+from mechestim.fft._transforms import (  # noqa: F401
+    fft, ifft, rfft, irfft,
+    fft2, ifft2, rfft2, irfft2,
+    fftn, ifftn, rfftn, irfftn,
+    hfft, ihfft,
+    fft_cost, rfft_cost, fftn_cost, rfftn_cost, hfft_cost,
+)
+from mechestim.fft._free import (  # noqa: F401
+    fftfreq, rfftfreq, fftshift, ifftshift,
+)
 from mechestim._registry import make_module_getattr as _make_module_getattr
+
+__all__ = [
+    "fft", "ifft", "rfft", "irfft",
+    "fft2", "ifft2", "rfft2", "irfft2",
+    "fftn", "ifftn", "rfftn", "irfftn",
+    "hfft", "ihfft",
+    "fftfreq", "rfftfreq", "fftshift", "ifftshift",
+]
 
 __getattr__ = _make_module_getattr(module_prefix="fft.", module_label="mechestim.fft")
