227c55eb56a9385e0872d42812a470a9427d58d7
Sharada Mohanty <mohanty@aicrowd.com>
2026-04-03 13:54:45 +0100

    feat: vendor opt_einsum core (parser, helpers, blas, types, testing)

---
+++ src/mechestim/_opt_einsum/__init__.py
@@ -0,0 +1,5 @@
+"""Symmetry-aware einsum contraction path optimizer.
+
+Forked from opt_einsum (https://github.com/dgasmith/opt_einsum).
+See LICENSE and NOTICE in this directory for attribution.
+"""
