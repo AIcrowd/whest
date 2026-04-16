ab3782005c5476448807c0457149ed02cb06010a
Sharada Mohanty <mohanty@aicrowd.com>
2026-04-16 19:46:56 +0200

    feat(engine): add singleton (|V|=1) weighted-Burnside regime

--- website/components/symmetry-aware-einsum-contractions/engine/regimes/index.js
+++ website/components/symmetry-aware-einsum-contractions/engine/regimes/index.js
@@ -1,8 +1,10 @@
 // website/components/symmetry-aware-einsum-contractions/engine/regimes/index.js
 import { bruteForceOrbitRegime } from './bruteForceOrbit.js';
 import { directProductRegime } from './directProduct.js';
+import { singletonRegime } from './singleton.js';
 
 export const MIXED_REGIMES = [
+  singletonRegime,
   directProductRegime,
   bruteForceOrbitRegime,
 ];
